c2476d5b5b82771b599b115c081a9f446550cbf5
japman_demo <japman006@gmail.com>
2026-04-19 15:29:23 +0530

    class for disjoint set

---
+++ Graphs/DisjointSet/disjointSetClass.java
@@ -0,0 +1,61 @@
+public class DisjointSet {
+    int[] parent ;
+    int[] rank ;
+
+    public DisjointSet(int n){
+        parent = new int[n];
+        rank = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i ;
+            rank[i] = 0 ;
+        }
+    }
+    public int findParent(int node){
+        if(node == parent[node]){
+            return node ;
+        }
+        return parent[node] = findParent(parent[node]) ;
+    }
+
+    public void union ( int u , int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if(pu == pv) return ;
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        }else if(rank[pv] > rank[pu]){
+            parent[pu] = pv ;
+        }else{
+            parent[pu] = pv ;
+            rank[pv]++ ;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DisjointSet ds = new DisjointSet(8);
+
+        ds.union(1, 2);
+        ds.union(2, 3);
+        ds.union(4, 5);
+        ds.union(6, 7);
+        ds.union(5, 6);
+
+        if(ds.findParent(3) == ds.findParent(7)){
+            System.out.println("same");
+        }else{
+            System.out.println("not same");
+        }
+
+        ds.union(3, 7);
+
+        if(ds.findParent(3) == ds.findParent(7)){
+            System.out.println("same");
+        }else{
+            System.out.println("not same");
+        }
+
+    }
+}
