90af1cd3ca41d9d41006f790bda82ed155c8ca87
japman_demo <japman006@gmail.com>
2026-04-19 15:30:48 +0530

    Solved again using DSU

---
+++ Graphs/DisjointSet/NumberOfProvincesByDSU.java
@@ -0,0 +1,59 @@
+class DisjointSet {
+    int[] parent ;
+    int[] rank ;
+
+    public DisjointSet(int n){
+        parent = new int[n];
+        rank = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i ;
+            rank[i] = 0 ;
+        }
+    }
+    public int findParent(int node){
+        if(node == parent[node]){
+            return node ;
+        }
+        return parent[node] = findParent(parent[node]) ;
+    }
+
+    public void union ( int u , int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if(pu == pv) return ;
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        }else if(rank[pv] > rank[pu]){
+            parent[pu] = pv ;
+        }else{
+            parent[pu] = pv ;
+            rank[pv]++ ;
+        }
+    }
+}
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length ;
+
+        DisjointSet ds = new DisjointSet(n);
+
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<n; j++){
+                if(isConnected[i][j] == 1){
+                    ds.union(i , j);
+                }
+            }
+        }
+
+        int count = 0 ;
+        for(int i = 0 ; i<n ; i++){
+            if(ds.findParent(i) == i){
+                count++ ;
+            }
+        }
+        return count ;
+    }
+}
