65c6ad2bc09df7401e1dd10b4f94b98905ab952c
japman_demo <japman006@gmail.com>
2026-06-04 01:48:21 +0530

    stocks with cooldown

---
+++ DP/DP-on-stocks/DP39-BestTimeToBuyAndSellStocks-WithCooldown.java
@@ -0,0 +1,133 @@
+// recursive solution ----------------------------------------------------------------------------------
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         return memoization(0 , 1 , prices);
+//     }
+//     public int memoization(int index , int buy , int[] prices){
+//         int n = prices.length ;
+//         int profit = 0 ;
+//         //base case 
+//         if(index >= n) return 0 ;
+
+//         //DP states - 
+//         if(buy == 1){
+//             int Bought = - prices[index] + memoization(index + 1 , 0 , prices);     //take case
+//             int notBought = 0 + memoization(index + 1 , 1 , prices);                //not take case
+//             profit = Math.max(Bought , notBought);
+//         }else{
+//             int Selled = prices[index] + memoization(index + 2 , 1 , prices);       //take case
+//             int notSelled = 0 + memoization( index + 1 , 0 , prices) ;              //not take case
+//             profit = Math.max(Selled , notSelled);
+//         }
+//         return profit ;
+//     }
+// }
+
+//memoization solution ---------------------------------------------------------------------------------
+
+
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length ;
+//         int[][] dp = new int[n][2] ;
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = 0 ; j < 2 ; j++){
+//                 dp[i][j] = -1 ;
+//             }
+//         }
+//         return memoization(0 , 1 , prices , dp);
+//     }
+//     public int memoization(int index , int buy , int[] prices , int[][] dp){
+//         int n = prices.length ;
+//         int profit = 0 ;
+//         //base case 
+//         if(index >= n) return 0 ;
+
+//         if(dp[index][buy] != -1) return dp[index][buy] ;
+
+//         //DP states - 
+//         if(buy == 1){
+//             int Bought = - prices[index] + memoization(index + 1 , 0 , prices , dp);     //take case
+//             int notBought = 0 + memoization(index + 1 , 1 , prices , dp);                //not take case
+//             profit = Math.max(Bought , notBought);
+//         }else{
+//             int Selled = prices[index] + memoization(index + 2 , 1 , prices , dp);       //take case
+//             int notSelled = 0 + memoization( index + 1 , 0 , prices , dp) ;              //not take case
+//             profit = Math.max(Selled , notSelled);
+//         }
+//         return dp[index][buy] =  profit ;
+//     }
+// }
+
+
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length ;
+//         int[][] dp = new int[n + 2][2] ;
+
+//         //base case -
+//         dp[n][0] = dp[n][1] = 0;
+//         dp[n+1][0] = dp[n+1][1] = 0;
+
+//         //loop 
+//         int profit = 0 ;
+//         for(int index = n-1 ; index >= 0 ; index--){
+//             for(int buy = 0 ; buy <= 1 ; buy++){
+//                 if(buy == 1){
+//                     int Bought = - prices[index] + dp[index + 1][0] ;     //take case
+//                     int notBought = 0 + dp[index + 1][1] ;                //not take case
+//                     profit = Math.max(Bought , notBought);
+//                 }else{
+//                     int Selled = prices[index] + dp[index + 2][1] ;       //take case
+//                     int notSelled = 0 + dp[index + 1][0] ;              //not take case
+//                     profit = Math.max(Selled , notSelled);
+//                 }
+//                 dp[index][buy] =  profit ;
+//             }
+//         }
+//         return dp[0][1] ;
+//     }
+// }
+
+
+
+
+
+
+
+//space optimized 
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int n = prices.length;
+
+        int[] ahead1 = new int[2]; // dp[index + 1]
+        int[] ahead2 = new int[2]; // dp[index + 2]
+        int[] curr = new int[2];
+
+        for (int index = n - 1; index >= 0; index--) {
+
+            // buy = 1
+            curr[1] = Math.max(
+                    -prices[index] + ahead1[0],
+                    ahead1[1]
+            );
+
+            // buy = 0
+            curr[0] = Math.max(
+                    prices[index] + ahead2[1], // cooldown
+                    ahead1[0]
+            );
+
+            ahead2 = ahead1.clone();
+            ahead1 = curr.clone();
+        }
+
+        return ahead1[1];
+    }
+}
