4bef526d8bb9b2c7acb2e4ce4f8b08e5730afde5
Japman Singh <japman006@gmail.com>
2026-08-07 22:43:10 +0530

    Time: 38 ms (64.97%), Space: 98.2 MB (72.89%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length ;
+        Stack<Integer> stack = new Stack<>();
+        int bestTwo = Integer.MIN_VALUE ;
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(nums[i] < bestTwo){
+                return true ;
+            }
+            while(!stack.isEmpty() && nums[i] > stack.peek()){
+                bestTwo = stack.pop();
+            }
+            stack.push(nums[i]);
+        }
+        return false ;
+    }
+}
